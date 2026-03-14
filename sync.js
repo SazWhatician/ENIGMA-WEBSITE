b3883550ffb27dd4734aadc7f2c40d798ba1e160
SazWhatician <saswatmohanty029@gmail.com>
2026-03-14 11:14:10 +0530

    refactor: Update team member information and section styles

--- sync.js
+++ sync.js
@@ -1,16 +1,36 @@
-require('dotenv').config(); // Add this at the top
-const { Client } = require('pg');
+require('dotenv').config(); 
+const { Client } = require('pg'); 
 
+
+
+
+
+
+
+//Add Projects here:
 const projects = [
     { title: "eNIGGmA", desc: "2 things you maybe wanna ride", img: "./project-assets/03ebb93dc76d5da51b66fa1826d3c548.jpg", link: "https://www.instagram.com/iamsazwat/?hl=en" },
     { title: "Kanye West", desc: "aesthetic cd picture from pinterest", img: "./project-assets/1937c631f87b36cdab243f6fe4d0bc07.jpg", link: "#" },
     { title: "TAWSAS", desc: "Saswat means someone who's Eternal- never ending", img: "./project-assets/abcba71e1e62c9efb1e98b462058ce4c.jpg", link: "https://www.google.com/?zx=1773264614308&no_sw_cr=1" },
     { title: "AmGGINe", desc: "for the girls: 2 things you maybe wanna ride", img: "./project-assets/bc06fb92df284696e7191a5b781b97f4.jpg", link: "#" },
+    { title: "Tyler- the creator", desc: "Tyler is really cool", img: "./project-assets/d7d287744fcbefbba4c0db2ac389cf2b.jpg", link: "#" },
     { title: "Tyler- the creator", desc: "Tyler is really cool", img: "./project-assets/d7d287744fcbefbba4c0db2ac389cf2b.jpg", link: "#" }
 ];
 
+
+
+
+
+
+
+
+
+
+
+
+
 async function syncDatabase() {
-    // Now it ONLY looks for the secure environment variable
+    
     const DB_URL = process.env.DATABASE_URL; 
     
     if (!DB_URL) {
